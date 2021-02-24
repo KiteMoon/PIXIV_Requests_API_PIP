588cc82f42b8ce8bd55614a29e081de9fbc0df5c
桜火 <xie@loli.fit>
2021-02-24 14:03:17 +0800

    增加注释
    Signed-off-by: 桜火 <xie@loli.fit>

--- Pixiv/down_img.py
+++ Pixiv/down_img.py
@@ -23,19 +23,23 @@ mysql_info_username = config_info.get("mysql", "username")
 mysql_info_password = config_info.get("mysql", "password")
 
 
+# 获取数据库信息
+
 # 打开数据库连接
 # 使用cursor()方法获取操作游标
 def get_all():
 	db = pymysql.connect(host=mysql_info_host, user=mysql_info_username, password=mysql_info_password,
 	                     database=mysql_info_name)
+	# 连接数据库
 	cursor = db.cursor()
-	# SQL 查询语句
+	# 遍历全表
 	sql = "select * from PIXIV_day"
-	# 执行SQL语句
+	# 执行语句
 	cursor.execute(sql)
 	# 获取所有记录列表
 	down_url = cursor.fetchall()
 	return down_url
+	# 断开
 	db.close()
 
 
@@ -49,20 +53,23 @@ for all in get_all():
 		"referer": "https://www.pixiv.net/",
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36 Edg/88.0.705.49"
 	}
-	img_pid = all[1]
-	img_down_url = all[7]
+	img_pid = all[1]  # 获取图片PID（从数据库中）
+	img_down_url = all[7]  # 获取图片下载地址（从数据库中）
 	img = requests.get(url=img_down_url, headers=headers)
-	print(img.status_code)
-	if img.status_code == 200:
+	# 上方为下载图片
+	print("当前状态码：" + str(img.status_code))  # 输出状态码
+	if img.status_code == 200:  # 判断是否获取成功
 		file_name = str(all[1]) + img_down_url[-4:]
-
 		print("开始保存图片，图片名称：" + file_name)
 		with open(("./img/" + file_name), "wb+") as files:
 			files.write(img.content)
+		# 保存图片到img路径
 		print("保存成功")
 		time_num = random.randint(1, 10)
 		print("系统开始休眠，下次运行时间：" + str(time_num) + "s")
 		time.sleep(time_num)
 		print("系统休眠结束,开始下一次运行")
+	# 随机休眠机制
 	else:
+		print("发生错误，图片下载失败，具体请查看状态码，系统将自动跳出循环并查询下一个图片")
 		continue
